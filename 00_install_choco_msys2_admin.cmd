ebe1363c4cb963d4faa1d32bab741b5dc24f1b5f
ppigazzini <pasquale.pigazzini@gmail.com>
2021-10-06 15:52:34 +0200

    Use the updated install command
    Update also the documentation link

--- 00_install_choco_msys2_admin.cmd
+++ 00_install_choco_msys2_admin.cmd
@@ -1,8 +1,7 @@
 @echo off
-::https://chocolatey.org/install
-::https://chocolatey.org/courses/installation/installing?method=installing-chocolatey?quiz=true
+::https://docs.chocolatey.org/en-us/choco/setup
 
 ::download and run install.ps1
-"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 ::install msys2
 choco install msys2 -y
