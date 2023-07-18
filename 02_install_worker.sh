712cdee1386b878628d821de1e28b2707d313f2e
ppigazzini <pasquale.pigazzini@gmail.com>
2023-07-18 11:47:13 +0200

    Improve bash code

--- 02_install_worker.sh
+++ 02_install_worker.sh
@@ -2,27 +2,23 @@
 # install and configure fishtest worker
 
 # print CPU information
-cpu_model=$(grep "^model name" /proc/cpuinfo | sort | uniq | cut -d ':' -f 2)
-n_cpus=$( grep "^physical id" /proc/cpuinfo | sort | uniq | wc -l)
-online_cores=$(grep "^bogo" /proc/cpuinfo | wc -l)
-n_siblings=$(grep "^siblings" /proc/cpuinfo | sort | uniq | cut -d ':' -f 2)
-n_cpu_cores=$(grep "^cpu cores" /proc/cpuinfo | sort | uniq | cut -d ':' -f 2)
-total_siblings=$((${n_cpus} * ${n_siblings}))
-total_cpu_cores=$((${n_cpus} * ${n_cpu_cores}))
-printf "CPU model : ${cpu_model}\n"
-printf "CPU       : %3d  -  Online cores    : %3d\n" ${n_cpus} ${online_cores}
-printf "Siblings  : %3d  -  Total siblings  : %3d\n" ${n_siblings} ${total_siblings}
-printf "CPU cores : %3d  -  Total CPU cores : %3d\n" ${n_cpu_cores} ${total_cpu_cores}
+cpu_model=$(grep "^model name" /proc/cpuinfo | uniq | cut -d ':' -f 2)
+n_cpus=$(grep "^physical id" /proc/cpuinfo | uniq | wc -l)
+online_cores=$(grep "^processor" /proc/cpuinfo | wc -l)
+n_siblings=$(grep "^siblings" /proc/cpuinfo | uniq | cut -d ':' -f 2)
+n_cpu_cores=$(grep "^cpu cores" /proc/cpuinfo | uniq | cut -d ':' -f 2)
+total_siblings=$(($n_cpus * $n_siblings))
+total_cpu_cores=$(($n_cpus * $n_cpu_cores))
+printf "CPU model : $cpu_model\n"
+printf "CPU       : %3d  -  Online cores    : %3d\n" $n_cpus $online_cores
+printf "Siblings  : %3d  -  Total siblings  : %3d\n" $n_siblings $total_siblings
+printf "CPU cores : %3d  -  Total CPU cores : %3d\n" $n_cpu_cores $total_cpu_cores
 
 # read fishtest credentials and number of cores to be contributed
 echo
-echo "Write your fishtest username:"
-read usr_name
-echo "Write your fishtest password:"
-read usr_pwd
-echo "Write the number of cores to be contributed to fishtest:"
-echo "(max suggested 'Total CPU cores - 1')"
-read n_cores
+read -p "Write your fishtest username: " usr_name
+read -p "Write your fishtest password: " usr_pwd
+read -p "Write the number of cores to be contributed to fishtest (max suggested 'Total CPU cores - 1'): " n_cores
 
 # install packages if not already installed
 pacman -S --noconfirm --needed unzip make mingw-w64-x86_64-gcc mingw-w64-x86_64-python3
@@ -40,7 +36,7 @@ python3.exe -m venv "env"
 env/bin/python3.exe -m pip install --upgrade pip setuptools wheel
 env/bin/python3.exe -m pip install requests
 # write fishtest.cfg
-env/bin/python3.exe worker.py "${usr_name}" "${usr_pwd}" --concurrency "${n_cores}" --only_config --no_validation && echo "Successfully set the concurrency value" || echo "Error: restart the script setting a proper concurrency value"
+env/bin/python3.exe worker.py "$usr_name" "$usr_pwd" --concurrency "$n_cores" --only_config --no_validation && echo "Successfully set the concurrency value" || echo "Error: restart the script setting a proper concurrency value"
 
 cat << EOF >> fishtest.cmd
 @echo off
@@ -50,5 +46,5 @@ env\bin\python3.exe -i worker.py
 EOF
 
 popd && popd
-mv ${tmp_dir}/fishtest-master/worker .
-rm -rf ${tmp_dir}
+mv $tmp_dir/fishtest-master/worker .
+rm -rf $tmp_dir
